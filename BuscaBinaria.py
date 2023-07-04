b4c651349cc7a2c1f018ae01efa1ed1c63212e81
Adson Henrique <80604595+adsonsilvah@users.noreply.github.com>
2023-07-04 20:20:07 -0300

    Criando a def procura

--- BuscaBinaria.py
+++ BuscaBinaria.py
@@ -1,37 +1,37 @@
 import numpy as np
 import random
 
-array = np.array([4,12,27,32,36])
-tamanhoArray = len(array)
-primeiroindice = 0
-ultimoindice = tamanhoArray-1
-indice_meio = (primeiroindice + ultimoindice)//2
-elemento_do_meio = array[indice_meio]
-numero = 17
-while True:
-    if primeiroindice == ultimoindice:
-        if elemento_do_meio != numero:
-            print("Não aparece na lista")
-            break
-    if numero == elemento_do_meio:
-        print(f"{elemento_do_meio} encontrado na posição {indice_meio}")
-        break
-
-    elif numero>elemento_do_meio:
-        novoindice = indice_meio + 1
-        primeiroindice = novoindice
-        ultimoindice = tamanhoArray - 1
-        indice_meio = (primeiroindice + ultimoindice)//2
-        elemento_do_meio = array[indice_meio]
+def procura(array, numero):
+    tamanhoArray = len(array)
+    primeiroindice = 0
+    ultimoindice = tamanhoArray-1
+    indice_meio = (primeiroindice + ultimoindice)//2
+    elemento_do_meio = array[indice_meio]
+    while True:
+        if primeiroindice == ultimoindice:
+            if elemento_do_meio != numero:
+                return f'{numero} não está no array'
         if numero == elemento_do_meio:
-            print(f"{elemento_do_meio} encontrado na posição {indice_meio}")
-            break
+            return f'posição no array: {indice_meio}'
 
-    elif numero<elemento_do_meio:
-        novoindice = indice_meio - 1
-        ultimoindice = novoindice
-        indice_meio = (primeiroindice + ultimoindice)//2
-        elemento_do_meio = array[indice_meio]
-        if numero == elemento_do_meio:
-            print(f"{elemento_do_meio} encontrado na posição {indice_meio}")
-            break
+        elif numero>elemento_do_meio:
+            novoindice = indice_meio + 1
+            primeiroindice = novoindice
+            ultimoindice = tamanhoArray - 1
+            indice_meio = (primeiroindice + ultimoindice)//2
+            elemento_do_meio = array[indice_meio]
+            if numero == elemento_do_meio:
+               return f'posição no array: {indice_meio}'
+
+        elif numero<elemento_do_meio:
+            novoindice = indice_meio - 1
+            ultimoindice = novoindice
+            indice_meio = (primeiroindice + ultimoindice)//2
+            elemento_do_meio = array[indice_meio]
+            if numero == elemento_do_meio:
+                return f'posição no array: {indice_meio}'
+
+array = np.arange(0,101)
+numero = random.randint(0,101)
+print(numero)
+print(procura(array, numero))
